8a23f616251d4d39f2755b1f22fbe7a8c0c56c24
udkazi <udkazi777@gmail.com>
2021-04-13 00:24:32 +0530

    Redux Bootstrap setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <nav className="navbar navbar-light bg-light">
+                <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    <img src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" className="d-inline-block align-text-top" />
+                    Redux details.
+                </a>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+
+
+// Store
+// Action type
+// Reducer
+// Dispatch
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
