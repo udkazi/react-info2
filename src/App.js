658e1231ab7a11d9863eaf93ab2aefc01c43dcd1
udkazi <udkazi777@gmail.com>
2021-04-13 22:13:01 +0530

    conditional Render view

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Table from './components/Table';
 
 function App() {
+
+
   return (
     <div className="App">
       
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,86 +1,47 @@
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
-function Table() {
+function Table(props) {
 
-    const [list, setList] = useState('');
+  const [list, setList] = useState('');
+  
+  useEffect(() => {
+    fetch('https://reqres.in/api/users?page=1')
+      .then(response => response.json())
+      .then(json => setList(json.data))
+  }, [])
 
-    useEffect(()=>{
-        fetch('https://reqres.in/api/users?page=1')
-        .then(response => response.json())
-        .then(json => setList(json.data))
-    })
+  const deleteMe = (id) => {
+    console.log('deleted - ID', id);
+  }
 
-    const data = [
-        {
-          "id": 7,
-          "email": "michael.lawson@reqres.in",
-          "first_name": "Michael",
-          "last_name": "Lawson",
-          "avatar": "https://reqres.in/img/faces/7-image.jpg"
-        },
-        {
-          "id": 8,
-          "email": "lindsay.ferguson@reqres.in",
-          "first_name": "Lindsay",
-          "last_name": "Ferguson",
-          "avatar": "https://reqres.in/img/faces/8-image.jpg"
-        },
-        {
-          "id": 9,
-          "email": "tobias.funke@reqres.in",
-          "first_name": "Tobias",
-          "last_name": "Funke",
-          "avatar": "https://reqres.in/img/faces/9-image.jpg"
-        },
-        {
-          "id": 10,
-          "email": "byron.fields@reqres.in",
-          "first_name": "Byron",
-          "last_name": "Fields",
-          "avatar": "https://reqres.in/img/faces/10-image.jpg"
-        },
-        {
-          "id": 11,
-          "email": "george.edwards@reqres.in",
-          "first_name": "George",
-          "last_name": "Edwards",
-          "avatar": "https://reqres.in/img/faces/11-image.jpg"
-        },
-        {
-          "id": 12,
-          "email": "rachel.howell@reqres.in",
-          "first_name": "Rachel",
-          "last_name": "Howell",
-          "avatar": "https://reqres.in/img/faces/12-image.jpg"
+  return (
+    <div className="container">
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+
+        {list.length > 0 &&
+          <tbody>
+            {list.map(ls => (
+              <tr key={ls.id}>
+                <th scope="row">{ls.id}</th>
+                <td>{ls.first_name}</td>
+                <td>{ls.email}</td>
+                <td><a onClick={(e) => deleteMe(ls.id, e)}>Delete</a></td>
+              </tr>
+            ))}
+          </tbody>
         }
-      ]
+      </table>
+    </div>
+  )
 
-    
-
-    return (
-        <div className="container">
-            <table className="table">
-                <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Action</th>
-                </tr>
-                </thead>
-                {data.map(ls => (
-                    <tbody>
-                    <tr key={ls.id}>
-                        <th scope="row">{ls.id}</th>
-                        <td>{ls.first_name}</td>
-                        <td>{ls.email}</td>
-                        <td><a>Delete</a></td>
-                    </tr>
-                    </tbody>
-                 ))}       
-            </table>
-        </div>
-    )
 }
 
 export default Table
